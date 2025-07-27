9c05793d700bd342b29fe80180cd53c089c480c7
Atul <atuljagtap876@gmail.com>
2025-07-27 18:06:28 +0530

    incorrect API URL

--- test-api.test.js
+++ test-api.test.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 describe('Sample API Test', () => {
   test('should return 200 OK', async () => {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts/1');
+    const response = await axios.get('https://jsonplaceholder1.typicode.com/posts/1');
     expect(response.status).toBe(200);
   });
 });
